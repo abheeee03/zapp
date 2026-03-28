28bacb862a0bd4449a4916ac43603da0f3104c33
Abhee <abhaynimbalkar03@gmail.com>
2026-03-28 11:31:40 +0530

    (feat) shadcn components

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div className="h-screen w-full flex flex-col gap-5 items-center justify-center">
+      <h1 className='text-4xl font-semibold'>Zapp</h1>
+      <p>short your links</p>
+    </div>
   )
 }
 
